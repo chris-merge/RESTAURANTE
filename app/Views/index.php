456e61a210b74eb2b13d6e026843ca9589a3ca56
chris-merge <chrishale1997j2@gmail.com>
2021-02-02 20:19:42 -0600

    Maquetacion de la pgina de inicio, esta maquetado mision, vision,nostros forms

---
+++ app/Views/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bienvenid@s</title>
+	 <link type="text/css" rel="stylesheet" href="/tutorial/css/materialize.min.css"  media="screen,projection"/>
+  <link rel="stylesheet" type="text/css" href="/tutorial/css/materialize.min.css" media="screen,projection"/>
+
+</head>
+<body>
+	<!--  -->
+	<!--ENCABEZADO DE LA PAGINA   -->
+	<h1> Comida Rapida "EL CHOCOLOCO"</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium exercitationem natus explicabo saepe minus debitis similique modi esse perspiciatis accusantium aspernatur aperiam dolorem placeat, fugiat, quibusdam repellat quia eaque vel.</p>
+		<!--  LOGIN -->
+	<h5>Para poder Ordnear Resgistrese </h3>
+	<form>
+    <div class="row">';
+    <div class="input-field ">';
+    <input id="name" type="text" class="validate" name="nombre">
+    <label class="active" for="name">Usuario</label>
+    </div>
+    <div class="input-field ">
+    <input name="pass" id="pass" type="password" class="validate">
+    <label class="active" for="pass">Contraceña</label>
+    </div>
+    <button  class='btn waves-effect waves-light' type='submit'>Ingresar</button>
+    <a href='#' class='waves-effect waves-light btn'>Registar</a>
+    </div>
+    </form>
+    <!-- FIN DE  FORMULARIO -->
+    <!-- Mision -->
+    <h3>Mision</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, laborum, cumque fuga natus laboriosam nemo iste illo repellendus repudiandae obcaecati eius fugiat explicabo vel. Quia nostrum minus doloribus assumenda, architecto?</p>
+    <!--Vision  -->
+    <h3>Vision</h3>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam laborum expedita sint perspiciatis, adipisci saepe rem vitae ut rerum esse eius, earum quasi reprehenderit voluptas ipsa blanditiis cupiditate tempora ipsum.</p>
+	<!-- MAS sobre Nosotros los creadores y produtores de la empresa  -->
+	<h2>Más Sobre Nosotros</h2>
+	<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem incidunt debitis maxime temporibus commodi in hic, excepturi repellendus aperiam, similique quidem qui fugiat dolorem nisi, optio non provident fuga quos.</p>
+	<!-- contactos de link-->
+	<a target="_blank" href="https://www.facebook.com/">Facebook</a>
+	<a target="_blank" href="https://chat.whatsapp.com/LiBJNhY0DF4HpohTSx42jB">Whatsapp</a>
+	<!-- DETALLE DE QUE SE VENTE  -->
+	<h2>La Mejor Comida</h2>
+	<p>Lorem ipsum dolor, sit, amet consectetur adipisicing elit. Aperiam, repudiandae tempore aspernatur repellat totam obcaecati magni accusantium exercitationem consequatur voluptatibus illum quae ipsa praesentium debitis quam est! Corrupti perferendis, cupiditate!</p>
+	<blockquote>
+		EL personal administrativo se compromete a ofrecer la mejor comida, con las medidas de proteccíon para garantizar la calidad de nuestros productos
+	</blockquote>
+	<P>-" MYPIME S.A De C.V"-</P>
+	<!--DERECHOS DE AUTOR  -->
+	<P>Todos lo derechos Reservados 2021 	&#169;</P>
+</body>
+<script type="text/javascript" src="/tutorial/js/materialize.min.js"></script>
+</html>
