6b309e740fbace282a7d1627e60add93187ef52f
chris-merge <chrishale1997j2@gmail.com>
2021-02-03 23:01:23 -0600

    se esta empezando a trabajr con css

--- app/Views/index.php
+++ app/Views/index.php
@@ -4,14 +4,23 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Bienvenid@s</title>
-	 <link type="text/css" rel="stylesheet" href="/tutorial/css/materialize.min.css"  media="screen,projection"/>
-  <link rel="stylesheet" type="text/css" href="/tutorial/css/materialize.min.css" media="screen,projection"/>
-
+	<!-- Refrencias de libreria  de materialize -->
+	<link type="text/css" rel="stylesheet" href="/tutorial/css/materialize.min.css"  media="screen,projection"/>
+	<!-- Refrencias de libreria  de materialize -->
+    <link rel="stylesheet" type="text/css" href="/tutorial/css/materialize.min.css" media="screen,projection"/>
+    <!-- link de css del estilo al index -->
+    <link rel="stylesheet" type="text/css" href="/tutorial/css/Estilo_index.css" media="screen,projection"/>
 </head>
+
 <body>
 	<!--  -->
+	<!-- HEADER DE LA PAGIAN PRINCIPAL  -->
 	<!--ENCABEZADO DE LA PAGINA   -->
+	<header id-side-header>
+		
 	<h1> Comida Rapida "EL CHOCOLOCO"</h1>
+	<!-- imagen de fondo -->
+	<img src="https://i.ibb.co/4NLdsks/Superfoods.jpg" alt="menu">
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium exercitationem natus explicabo saepe minus debitis similique modi esse perspiciatis accusantium aspernatur aperiam dolorem placeat, fugiat, quibusdam repellat quia eaque vel.</p>
 		<!--  LOGIN -->
 	<h5>Para poder Ordnear Resgistrese </h3>
@@ -29,28 +38,53 @@
     <a href='#' class='waves-effect waves-light btn'>Registar</a>
     </div>
     </form>
+    </header>
+    <!-- FIN DE HEADER DE LA PAGIAN PRINCIPAL  -->
     <!-- FIN DE  FORMULARIO -->
+    <!-- SECCION DE MISION VISSION nosotros  -->
+    <section>
     <!-- Mision -->
+    <div>
     <h3>Mision</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, laborum, cumque fuga natus laboriosam nemo iste illo repellendus repudiandae obcaecati eius fugiat explicabo vel. Quia nostrum minus doloribus assumenda, architecto?</p>
+    </div>
     <!--Vision  -->
+    <div>
     <h3>Vision</h3>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam laborum expedita sint perspiciatis, adipisci saepe rem vitae ut rerum esse eius, earum quasi reprehenderit voluptas ipsa blanditiis cupiditate tempora ipsum.</p>
-	<!-- MAS sobre Nosotros los creadores y produtores de la empresa  -->
-	<h2>Más Sobre Nosotros</h2>
-	<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem incidunt debitis maxime temporibus commodi in hic, excepturi repellendus aperiam, similique quidem qui fugiat dolorem nisi, optio non provident fuga quos.</p>
-	<!-- contactos de link-->
-	<a target="_blank" href="https://www.facebook.com/">Facebook</a>
-	<a target="_blank" href="https://chat.whatsapp.com/LiBJNhY0DF4HpohTSx42jB">Whatsapp</a>
+    </div>
 	<!-- DETALLE DE QUE SE VENTE  -->
-	<h2>La Mejor Comida</h2>
+	<section>
+		<article>
+	<h3>La Mejor Comida</h3>
 	<p>Lorem ipsum dolor, sit, amet consectetur adipisicing elit. Aperiam, repudiandae tempore aspernatur repellat totam obcaecati magni accusantium exercitationem consequatur voluptatibus illum quae ipsa praesentium debitis quam est! Corrupti perferendis, cupiditate!</p>
+	</article>
+	</section>
+	<!-- DETALLE DE QUE SE VENTE  -->
+	<section>
 	<blockquote>
 		EL personal administrativo se compromete a ofrecer la mejor comida, con las medidas de proteccíon para garantizar la calidad de nuestros productos
 	</blockquote>
 	<P>-" MYPIME S.A De C.V"-</P>
-	<!--DERECHOS DE AUTOR  -->
-	<P>Todos lo derechos Reservados 2021 	&#169;</P>
+	</section>
 </body>
+
+	<!-- MAS sobre Nosotros los creadores y produtores de la empresa  -->
+	<footer>
+	<nav>
+		<a href="#">Nosotros</a>
+		&nbsp;
+		<a href="#">Desarrolladores</a>
+		&nbsp;
+		<a href="#">Blog</a>
+		&nbsp;
+		&nbsp;
+		&nbsp;
+		<a href="#">Todos lo derechos Reservados 2021 	&#169;</a>
+	</nav>
+	<!--DERECHOS DE AUTOR  -->
+	
+	<!-- SECCION DE MISION VISSION nosotros  -->
+	</footer>
 <script type="text/javascript" src="/tutorial/js/materialize.min.js"></script>
 </html>
