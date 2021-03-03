43877992290a359219064665191f8c52df209a8a
chris-merge <chrishale1997j2@gmail.com>
2021-03-03 00:03:23 -0600

    se arreglo el nav, se puso icons de materialice, se puso la barra de carga como decoracion, se uso flex-box en la mision,vision!

--- app/Views/index.php
+++ app/Views/index.php
@@ -10,34 +10,86 @@
     <link rel="stylesheet" type="text/css" href="/tutorial/css/materialize.min.css" media="screen,projection"/>
     <!-- link de css del estilo al index -->
     <link rel="stylesheet" type="text/css" href="/tutorial/css/Estilo_index.css" media="screen,projection"/>
+    <!--  Link de Materialize -->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap" rel="stylesheet">
 </head>
 
 <body>
 	<!--  -->
+	<nav id="navegacion" >
+    <div class="nav-wrapper">
+      <ul class="right">
+      	<!-- ICONO  -->
+      	<li>
+      		<i class="material-icons">account_circle</i>
+      	</li>
+      	<!-- LINK PARA REGRESAR EL INICIO -->
+      <li>
+      	<a href="#">Login</a>
+      </li>
+      <!--ICONS DE NOSOTROS  -->
+      <li>
+      	<i class="material-icons">perm_contact_calendar</i>
+      </li>
+      <li>
+      	<a href="#">Nosotros</a>
+      </li>
+      <!-- ICONS DE BLOG -->
+      <li>
+      	<i class="material-icons">textsms</i>
+      </li>
+      <li>
+      	<a href="#">BLOG</a>
+      </li>
+      <!-- ICOS DE CONTACTO -->
+      <li>
+      	<i class="material-icons">contact_mails</i>
+      </li>
+      <li>
+      	<a href="#">Contacto</a>
+      </li>
+      </ul>
+    </div>
+  </nav>
+  <!--  -->
+  <!-- barra de proceso adorno -->
+  <div class="progress">
+      <div class="indeterminate"></div>
+  </div>
 	<!-- HEADER DE LA PAGIAN PRINCIPAL  -->
-	
+		
 	<!--ENCABEZADO DE LA PAGINA   -->
 	<header class="side-header inicio">
-		
-	<h1> !COMIDA RAPIDA!</h1>		
+		<!-- el side bar-->
+		<h1 id="h1"> !COMIDA RAPIDA!</h1>	
+		<div id="id_logo">
+			
+		</div>
     </header>
+    
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium exercitationem natus explicabo saepe minus debitis similique modi esse perspiciatis accusantium aspernatur aperiam dolorem placeat, fugiat, quibusdam repellat quia eaque vel.</p>
     <!-- FIN DE HEADER DE LA PAGIAN PRINCIPAL  -->
     <!-- FIN DE  FORMULARIO -->
     <!-- SECCION DE MISION VISSION nosotros  -->
     <section>
     <!-- Mision -->
+    <div class="barra">
     <div>
-    <h3>Mision</h3>
+    <h3 id="Mision">Mision</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, laborum, cumque fuga natus laboriosam nemo iste illo repellendus repudiandae obcaecati eius fugiat explicabo vel. Quia nostrum minus doloribus assumenda, architecto?</p>
     </div>
     <!--Vision  -->
     <div>
-    <h3>Vision</h3>
+    <h3 id="Vision">Vision</h3>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam laborum expedita sint perspiciatis, adipisci saepe rem vitae ut rerum esse eius, earum quasi reprehenderit voluptas ipsa blanditiis cupiditate tempora ipsum.</p>
+    </div>
     </div>
 	<!-- DETALLE DE QUE SE VENTE  -->
 	<section>
+		
+		<!--ARTICULO DE LA PAGINA  -->
 		<article>
 	<h3>La Mejor Comida</h3>
 	<p>Lorem ipsum dolor, sit, amet consectetur adipisicing elit. Aperiam, repudiandae tempore aspernatur repellat totam obcaecati magni accusantium exercitationem consequatur voluptatibus illum quae ipsa praesentium debitis quam est! Corrupti perferendis, cupiditate!</p>
@@ -54,16 +106,14 @@
 
 	<!-- MAS sobre Nosotros los creadores y produtores de la empresa  -->
 	<footer>
-	<nav>
-		<a href="#">Nosotros</a>
-		&nbsp;
+	<nav id="footer">
+		<ul>
+		<li>
 		<a href="#">Desarrolladores</a>
-		&nbsp;
-		<a href="#">Blog</a>
-		&nbsp;
-		&nbsp;
-		&nbsp;
-		<a href="#">Todos lo derechos Reservados 2021 	&#169;</a>
+		</li>
+		<li><a href="#">Todos lo derechos Reservados 2021</a>
+		</li>
+		</ul>
 	</nav>
 	<!--DERECHOS DE AUTOR  -->
 	
